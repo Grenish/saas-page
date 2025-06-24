28261d4cad62200d56c49654dd7d216edcddfeac
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-06-25 01:26:02 +0530

    Refactor layout.tsx to use Montserrat font and improve structure with Navbar and SmoothScrollWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import SmoothScrollWrapper from "@/components/smooth-scroll-wrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const Mont = Montserrat({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${Mont.className} antialiased`}>
+        <Navbar />
+        <SmoothScrollWrapper>{children}</SmoothScrollWrapper>
       </body>
     </html>
   );
